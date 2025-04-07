2f3e6cf521997e9b9a3d2fc1ea2e52cf2168796f
zafkiel45 <zafkielc@gmail.com>
2025-04-07 11:25:49 -0300

    feat: added local connection

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import express from 'express';
+import { docsRoute } from './routes/docs.router';
+
+const route = express();
+
+route.use('/docs', docsRoute);
+
+route.listen(3001, () => {
+    console.log('tudo funcionando!');
+});
