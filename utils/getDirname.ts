c0d0e7cc0568c05bbcf6c988e2c7314e836081df
zafkiel45 <zafkielc@gmail.com>
2025-04-08 14:06:01 -0300

    fix: fixed repeated folders on the frontend

--- utils/getDirname.ts
+++ utils/getDirname.ts
@@ -2,14 +2,19 @@ import path from "node:path";
 
 const basePath = path.join("D:", "mdx-files");
 
-export function getDirname(pathArr: string[], pathBase = basePath): string[] {
+export function getDirname(pathArr: string[]): string[] {
   const paths: string[] = [];
 
   for (let file of pathArr) {
     if (!file.endsWith(".mdx")) continue;
+    if (paths.includes(convertPathToDirname(file))) continue;
 
-    paths.push(path.basename(path.dirname(path.join(pathBase, file))));
-  }
-
-  return paths;
+    paths.push(convertPathToDirname(file));
+  };
+  console.log(paths)
+  return paths
 }
+
+function convertPathToDirname(targetPath: string) {
+  return path.basename(path.dirname(path.join(basePath, targetPath)))
+};
