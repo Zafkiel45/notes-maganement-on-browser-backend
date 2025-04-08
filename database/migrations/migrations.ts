4ed42aa96797faa0b82f79dd0e4d5d812bec1f1f
zafkiel45 <zafkielc@gmail.com>
2025-04-08 11:14:11 -0300

    feat: names of files now are unique

--- database/migrations/migrations.ts
+++ database/migrations/migrations.ts
@@ -3,7 +3,7 @@ import { database } from "../config/config";
 database.run(`
     CREATE TABLE IF NOT EXISTS docs (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
+        name TEXT NOT NULL UNIQUE,
         content BLOB NOT NULL,
         type TEXT NOT NULL
     )    
