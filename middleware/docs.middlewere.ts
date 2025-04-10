c071216e1f055d2de6205aef796dfef8f9972d50
zafkiel45 <zafkielc@gmail.com>
2025-04-10 15:50:15 -0300

    fix: fixed wrong return from cors

--- middleware/docs.middlewere.ts
+++ middleware/docs.middlewere.ts
@@ -4,14 +4,15 @@ export function implementCors(
   req: Request,
   res: Response,
   next: NextFunction
-) {
+): void {
     res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Credentials', 'false');
     res.setHeader('Access-Control-Allow-Headers', '');
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
     if(req.method === 'OPTIONS') {
-      return res.sendStatus(204); // No Content
+      res.sendStatus(204); // No Content
+      return ; // there is a bug here when I try return the "res.sendStatus()"
     };
 
     next();
