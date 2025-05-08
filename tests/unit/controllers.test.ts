4d81d49f8fa4b95098bf9369936994417418a066
zafkiel45 <zafkielc@gmail.com>
2025-05-08 10:14:49 -0300

    test(controllers): implemented 'each' test for cover many possibilites

--- tests/unit/controllers.test.ts
+++ tests/unit/controllers.test.ts
@@ -2,16 +2,18 @@ import { test, mock, expect } from 'bun:test';
 import { createFolder } from '../../controllers/folders.controller';
 import { createFolderService, getFoldersByName } from '../../services/folder.services';
 
-mock.module('../../services/folder.services', () => ({
-  createFolderService: mock(),
-  getFoldersByName: mock(),
-}));
+const invalidCases = [null, 1, -1, 0, '', undefined];
 
-test('prevent folderCreateService to be called with invalid name', () => {
-  const req = {body: { folderName: 1} as any} as any;
+test.each(invalidCases)('createFolder returns 400 code with %p', (c) => {
+  mock.module('../../services/folder.services', () => ({
+    createFolderService: mock(),
+    getFoldersByName: mock(),
+  }));
+
+  const req = {body: { folderName: c} as any} as any;
   const sendStatus = mock();
   const res = { sendStatus } as any;
-  
+  console.log(c);
   createFolder(req, res);
 
   expect(sendStatus).toHaveBeenCalledWith(400);
